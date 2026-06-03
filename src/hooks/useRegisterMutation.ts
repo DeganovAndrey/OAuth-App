404c49183f3a289ce8572d013c2e981bb014e426
DeganovAndrey <dyoosha@gmail.com>
2026-06-03 11:38:07 +0300

    changes_3

--- src/hooks/useRegisterMutation.ts
+++ src/hooks/useRegisterMutation.ts
@@ -2,22 +2,30 @@ import { useMutation } from "@tanstack/react-query";
 import { useNavigate } from "react-router-dom";
 import { registerUser } from "../api/api";
 import type { Payload } from "../types";
-import type { AxiosError } from "axios";
+import { isAxiosError } from "axios";
+import { useState } from "react";
 
 export const useRegisterMutation = () => {
+  const [errorMessage, setErrorMessage] = useState<string>("");
+
   const navigate = useNavigate();
 
   const mutation = useMutation({
     mutationFn: (payload: Payload) => registerUser(payload),
-    onSuccess: () => navigate("/login"),
+    onSuccess: () => navigate("/dashboard"),
     onError: (error) => {
-      const axiosError = error as AxiosError;
-      if (axiosError.response?.status === 400) return "Плохой запрос клиента";
-      else if (axiosError.response?.status === 409) return "Email уже занят";
-      else if (axiosError.response?.status === 422)
-        return "Проверьте правильность заполнения полей";
-      else return "Неизвестная ошибка";
+      if (isAxiosError(error)) {
+        if (error.response?.status === 400)
+          setErrorMessage("Плохой запрос клиента");
+        else if (error.response?.status === 409)
+          setErrorMessage("Email уже занят");
+        else if (error.response?.status === 422)
+          setErrorMessage("Проверьте правильность заполнения полей");
+        else setErrorMessage(`Неизвестная ошибка - ${error.message}`);
+      }
     },
   });
-  return { ...mutation };
+  return { ...mutation, errorMessage };
 };
+
+//  eve.holt@reqres.in
